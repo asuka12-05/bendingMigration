d827a6a08d10e79d8adc8897fd35583e7fe48af2
mywork_asuka-admin <hane38mogi27@gmail.com>
2026-08-21 09:29:13 +0900

    first commit

---
+++ src/main/java/com/example/demo/BendingMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BendingMigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BendingMigrationApplication.class, args);
+	}
+
+}
